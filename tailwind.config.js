83c17ec198dccddde86a46c6e589ffe7c902abdd
KT022 <khanhphucphuc01@gmail.com>
2022-05-31 21:17:57 +0700

    New commit messag1233e

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors:{
+      'b-orange': 'hsl(31, 77%, 52%)',
+      'd-cyan' : 'hsl(184, 100%, 22%)',
+      'vd-cyan': 'hsl(179, 100%, 13%)',
+      't-white': 'hsla(0, 0%, 100%, 0.75)',
+      'l-gray': 'hsl(0, 0%, 95%)',
+    },
+    extend: {
+      fontFamily:{
+        'bigsd' : '"Big Shoulders Display", cursive',
+        'red': '"Lexend Deca", sans-serif',
+      },
+    },
+  },
+  plugins: [],
+}
